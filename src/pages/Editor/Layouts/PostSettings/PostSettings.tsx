4b79fcbab284e09e5cff65154d216f0a3aa2ce13
Alaeddin BENSALAH <alaeddinbs@gmail.com>
2023-04-26 21:17:50 +0100

    add basic tab in settings panel

--- src/pages/Editor/Layouts/PostSettings/PostSettings.tsx
+++ src/pages/Editor/Layouts/PostSettings/PostSettings.tsx
@@ -1,14 +1,59 @@
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { FiSettings, FiList, FiCalendar, FiUsers } from 'react-icons/fi';
+
 import styles from './PostSettings.module.scss';
 
-type PostSettingsProps = {};
+const PostSettings: React.FC = () => {
+    const [tabIndex, setTabIndex] = useState(0);
 
-function PostSettings(props: PostSettingsProps) {
+    const handleTabSelect = (index: number) => {
+        setTabIndex(index);
+    };
+
+    const isTabActive = (index: number): boolean => tabIndex === index;
+
     return (
         <div className={styles.postSettings}>
-            <h2>Post Settings</h2>
-            {/* Add your post settings controls here */}
+            <Tabs
+                selectedIndex={tabIndex}
+                onSelect={handleTabSelect}
+                className={styles.tabContainer}
+            >
+                <TabList className={styles.tabList}>
+                    <Tab className={`${styles.tab} ${isTabActive(0) ? styles.active : ''}`}>
+                        <FiSettings className={styles.tabIcon} />
+                        <span>Settings</span>
+                    </Tab>
+                    <Tab className={`${styles.tab} ${isTabActive(1) ? styles.active : ''}`}>
+                        <FiList className={styles.tabIcon} />
+                        <span>Templates</span>
+                    </Tab>
+                    <Tab className={`${styles.tab} ${isTabActive(2) ? styles.active : ''}`}>
+                        <FiCalendar className={styles.tabIcon} />
+                        <span>TBD</span>
+                    </Tab>
+                    <Tab className={`${styles.tab} ${isTabActive(3) ? styles.active : ''}`}>
+                        <FiUsers className={styles.tabIcon} />
+                        <span>TBD</span>
+                    </Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Settings Panel</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Template List Panel</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>TBD Panel</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>TBD Panel</h2>
+                </TabPanel>
+            </Tabs>
         </div>
     );
-}
+};
 
 export default PostSettings;
