3e3661fa3813010b472fc955b47bfd9ce016d9e5
Bilel chehata <chehata.b8@gmail.com>
2023-05-08 16:00:20 +0100

    Disable Redux/toolkit unserializable objects checking

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,6 +5,10 @@ export const store = configureStore({
   reducer: {
     templatesStore: TemplatesStore,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
