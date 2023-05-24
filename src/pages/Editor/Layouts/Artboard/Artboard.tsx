ccbec32afb79b5b6f201a1258b25487fe8ee13ad
Alaeddin BENSALAH <alaeddinbs@gmail.com>
2023-05-24 21:27:29 +0100

    add template info tooltip

--- src/pages/Editor/Layouts/Artboard/Artboard.tsx
+++ src/pages/Editor/Layouts/Artboard/Artboard.tsx
@@ -5,7 +5,9 @@ import styles from "./Artboard.module.scss";
 import { useAppSelector } from "../../../../app/hooks";
 import { getTemplatesStore } from "../../../../app/store/TemplatesStore";
 import Preview from "../../../../components/Preview/Preview";
-import { FiClipboard   } from 'react-icons/fi';
+import {FiChevronDown, FiChevronUp, FiClipboard, FiCode, FiFileText, FiInfo, FiShare2, FiUpload} from 'react-icons/fi';
+import {Tooltip} from "react-tooltip";
+import classNames from "classnames";
 
 function Artboard(props: { editor: string })  {
   const templateLoader = useAppSelector(getTemplatesStore);
@@ -18,7 +20,24 @@ function Artboard(props: { editor: string })  {
         </div>
 
         <div className={styles.artBoardInner}>
-            <h2 className={styles.templateTitle}><FiClipboard  />{templateLoader.currentTemplate.name ?? "undefined"}</h2>
+            <Tooltip isOpen={true} className={styles.templateInfo} id="template-info" place="left" delayHide={1000} delayShow={1000}>
+                <div className={classNames(styles.templateInfoTitle, styles.templateInfoRow)}>
+                    <h2>{templateLoader.currentTemplate.name}
+                    <span>{templateLoader.currentTemplate.version}</span>
+                    </h2>
+                </div>
+                <div className={classNames(styles.templateInfoDescription, styles.templateInfoRow) }>
+                    <p>{templateLoader.currentTemplate.description}
+                    </p>
+                </div>
+                <div className={styles.templateInfoRow}>
+                    <h3>{templateLoader.currentTemplate.height}px / {templateLoader.currentTemplate.width}px</h3>
+                </div>
+            </Tooltip>
+
+            <h2 data-tooltip-id="template-info" className={styles.templateTitle}>
+                <FiInfo className={styles.templateInfoButton} />
+                {templateLoader.currentTemplate.name ?? "undefined"}</h2>
             <Preview editor={props.editor} template={templateLoader.currentTemplate} />
         </div>
     </div>
