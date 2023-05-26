6573cc01a9f05f71cbcc9c2f5257d90af1af5eeb
Mr-Chehata <bilel.chehata@wallpostdev.com>
2023-05-26 10:51:56 +0100

    Add more allowed Tags in sanitizer config

--- src/config/render.ts
+++ src/config/render.ts
@@ -1,8 +1,15 @@
 export const SANITIZE_OPTIONS = {
-  allowedTags: ["b", "i", "em", "strong", "a", "div", "p", "h2"],
+  allowedTags: ["b", "i", "em", "strong", "a", "div", "p","h1", "h2","h3","h4","h5","h6"],
   allowedAttributes: {
     a: ["href"],
-    div: ["style", "class"],
+    div: ["id","style", "class"],
+    p: ["id","style", "class"],
+    h1: ["id","style", "class"],
+    h2: ["id","style", "class"],
+    h3: ["id","style", "class"],
+    h4: ["id","style", "class"],
+    h5: ["id","style", "class"],
+    h6: ["id","style", "class"],
   },
-  allowedIframeHostnames: ["www.youtube.com"],
+  allowedIframeHostnames: [],
 };
