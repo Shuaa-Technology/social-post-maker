8ed16702f9f91402c5dfb27c019710ebf456520b
Alaeddin BENSALAH <alaeddinbs@gmail.com>
2023-06-11 12:49:00 +0100

    update config for netlify

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
-export const API_ENABLED = process.env.API_ENABLED || true;
+export const API_ENABLED = process.env.API_ENABLED || false;
 export const API_URL = process.env.API_URL || "http://localhost";
 export const API_PORT = process.env.API_PORT || 3000;
