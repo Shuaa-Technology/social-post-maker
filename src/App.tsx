b75f57764dff42cfd4b1d776c1c6147f5ec0d17c
Alaeddin BENSALAH <alaeddinbs@gmail.com>
2023-06-18 15:41:45 +0100

    pop a warning when widht is less than 1000

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,49 @@ import EditorPage from "./pages/Editor/EditorPage";
 import { Helmet } from "react-helmet";
 import { getCurrentThemeMode } from "./app/store/ConfigStore";
 import { TemplateRenderer } from "./core/TemplateRenderer";
+import {useEffect, useState} from "react";
 
 function App() {
-  const currentTemplate = useAppSelector(getCurrentTemplate); //@todo pass current template to other child or find another way to get current template style
-  const currentThemeMode = useAppSelector(getCurrentThemeMode);
+    const [isMobile, setIsMobile] = useState(false);
+    const currentTemplate = useAppSelector(getCurrentTemplate); //@todo pass current template to other child or find another way to get current template style
+    const currentThemeMode = useAppSelector(getCurrentThemeMode);
 
   const style = new TemplateRenderer()
     .setTemplate(currentTemplate)
     .parse()
     .style();
 
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setIsMobile(window.innerWidth < 1000);
+        };
+
+        // Attach the resize event listener
+        window.addEventListener('resize', handleWindowResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
   return (
-    <div className={styles[currentThemeMode.handle]}>
-      <Helmet>
-        <style type="text/css">{style}</style>
-      </Helmet>
-      <EditorPage />
-    </div>
+      <>
+          {isMobile ? (
+              <div className={styles.warningOverlay}>
+                  <p>Oops! This app is designed for tablets and larger screens. For the best experience, please switch to a tablet or desktop device.</p>
+              </div>
+          ) : (
+              <div className={styles[currentThemeMode.handle]}>
+                  <Helmet>
+                      <style type="text/css">{style}</style>
+                  </Helmet>
+                  <div className={styles.mobileContainer}>
+                      <EditorPage />
+                  </div>
+              </div>
+          )}
+      </>
   );
 }
 
